a12136107f58bee81f829a0b75b7fb6ac0f8efc8
ellenlauredo <160611884+ellenlauredo@users.noreply.github.com>
2024-03-20 10:03:37 -0300

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,42 @@
+print ("Ellen")
+
+numero = 10
+nome = "clacla"
+booleano = True
+a = 10
+b = 5
+soma = a + b
+subtração = a - b
+multiplicação = a * b
+divisão = a / b
+c = 5
+d = 6
+e = 7
+f = 8
+frase = "tudo bem"
+palavra = "feliz"
+media = (a + b + c + d) / 4
+print ("A media é", media)
+
+quadrado = c * c
+print ("O quadrado do numero 5 é", quadrado)
+
+dobro = c * 2
+print ("O dobro do numero 5 é", dobro)
+
+frase = "tudo bem"
+quantidade_letras = len(frase)
+print ("A frase tem", quantidade_letras, "letras.")
+
+quantidade_espaços = frase.count (" ")
+print ("A quantidade de espaços na frase é", quantidade_espaços)
+
+if c > d:
+    print ("O c é maior que o d.")
+
+else:
+    print ("o d é maior que a.")
+
+maior_numero = max (c,d,e,f)
+print ("O maior numero é:", maior_numero)
+    
