ff67e0d996af6186dedddd4df8596498077b8b91
ellenlauredo <160611884+ellenlauredo@users.noreply.github.com>
2024-05-15 10:21:01 -0300

    Add files via upload

---
+++ ellen.py
@@ -0,0 +1,7 @@
+def bombons(diheiro, preço):
+    n_bombons = dinheiro // preco
+    troc = diheiro % preco
+    return n_bombons, troco
+
+def a_mais (dinheiro, preco) :
+    troco = bombons (dinheiro, preco)
